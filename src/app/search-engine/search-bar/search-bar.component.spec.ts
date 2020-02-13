63f77db1b8300bf876b8e8c334b49319506815d9
Luis Mendieta <lmendieta@nearsoft.com>
2020-02-13 13:55:36 -0600

    test: add search-bar testing

--- src/app/search-engine/search-bar/search-bar.component.spec.ts
+++ src/app/search-engine/search-bar/search-bar.component.spec.ts
@@ -1,24 +1,38 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SearchBarComponent } from './search-bar.component';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 describe('SearchBarComponent', () => {
   let component: SearchBarComponent;
   let fixture: ComponentFixture<SearchBarComponent>;
+  let de: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [SearchBarComponent]
+      declarations: [SearchBarComponent],
+      imports: [FormsModule]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(SearchBarComponent);
     component = fixture.componentInstance;
+    de = fixture.debugElement;
+    spyOn(component.submitSearch, 'emit');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should emit an submitSearch event', () => {
+    const form = de.query(By.css('form'));
+    component.inputValue = 'Hello World';
+    form.nativeElement.dispatchEvent(new Event('submit'));
+    expect(component.submitSearch.emit).toHaveBeenCalledWith('Hello World');
   });
 });
