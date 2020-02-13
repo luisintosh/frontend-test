6baa9224a75853e83953e260f3de15c1b1ceca87
Luis Mendieta <lmendieta@nearsoft.com>
2020-02-13 16:32:30 -0600

    test: add e2e tests

--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,27 @@
 import { browser, by, element } from 'protractor';
 
 export class AppPage {
-  navigateTo(): Promise<unknown> {
-    return browser.get(browser.baseUrl) as Promise<unknown>;
+  navigateTo(url: string): Promise<unknown> {
+    return browser.get(url) as Promise<unknown>;
   }
 
-  getTitleText(): Promise<string> {
-    return element(by.css('app-root .content span')).getText() as Promise<string>;
+  getNavbar() {
+    return element(by.css('nav .navbar .navbar-brand img'));
+  }
+
+  getSearchInput() {
+    return element(by.css('form input[name="search"]'));
+  }
+
+  getGalleryImages() {
+    return element.all(by.css('.gallery-container .card-columns .card img'));
+  }
+
+  getGalleryCardColumnsClass() {
+    return element(by.css('.gallery-container .card-columns'));
+  }
+
+  getPaginatorNextPageItem() {
+    return element(by.css('ngb-pagination > ul > li:last-child > a.page-link'));
   }
 }
